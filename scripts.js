8d7a0ba010386015707ff513bc85f78669ddc6dd
Aadecoder <adityandr8274@gmail.com>
2025-07-18 22:38:01 +0530

    did basic layout

---
+++ scripts.js
@@ -0,0 +1,6 @@
+const openInventory = document.querySelector(".open-inventory");
+const openJournal = document.querySelector(".open-journal");
+
+openInventory.addEventListener("click", ()=>{
+    window.location.href = "inventory.html";
+})
